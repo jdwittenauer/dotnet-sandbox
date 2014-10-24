12e50fb0298e7f2532df5ac80a98eca7b54f87fa
John Wittenauer <jdwittenauer@gmail.com>
2014-10-24 12:43:13 -0400

    Added more examples for reactive extensions.

--- Sandbox/Sandbox.Reactive/RxExamples.cs
+++ Sandbox/Sandbox.Reactive/RxExamples.cs
@@ -5,6 +5,7 @@ using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sandbox.Reactive.DictionaryService;
 
 namespace Sandbox.Reactive
 {
@@ -22,6 +23,8 @@ namespace Sandbox.Reactive
             Exercise1();
             Exercise2();
             Exercise3();
+            Exercise4();
+            Exercise5();
 
             Console.WriteLine("Example complete. Press a key to proceed.");
             Console.ReadKey();
@@ -101,6 +104,74 @@ namespace Sandbox.Reactive
 
             Console.WriteLine("Exercise 3 complete.");
             Console.ReadLine();
+        }
+
+        private static void Exercise4()
+        {
+            // Text box example demonstrating several new operators
+            var text = new TextBox();
+            var form = new Form
+            {
+                Controls = { text }
+            };
+
+            var input = Observable.FromEventPattern<EventArgs>(text, "TextChanged")
+                .Select(e => ((TextBox)e.Sender).Text)
+                .Throttle(TimeSpan.FromSeconds(1))
+                .Do(i => Console.WriteLine("Before DistinctUntilChanged: " + i))
+                .DistinctUntilChanged();
+
+            using (input.Subscribe(i => Console.WriteLine("User wrote: " + i)))
+            {
+                Application.Run(form);
+            }
+
+            Console.WriteLine("Exercise 4 complete.");
+            Console.ReadLine();
+        }
+
+        private static void Exercise5()
+        {
+            // Complete example using an external web service as an observable collection
+            var text = new TextBox();
+            var listBox = new ListBox { Top = text.Height + 10 };
+            var form = new Form { Controls = { text, listBox } };
+            
+            // Turn the user input into a tamed sequence of strings
+            var textChanged = from evt in Observable.FromEventPattern<EventArgs>(text, "TextChanged") 
+                              select ((TextBox)evt.Sender).Text;
+
+            var input = textChanged
+                .Throttle(TimeSpan.FromSeconds(1))
+                .DistinctUntilChanged();
+            
+            // Bridge with the web service's MatchInDict method
+            var service = new DictServiceSoapClient("DictServiceSoap");
+            var matchInDict = Observable.FromAsyncPattern<string, string, string, DictionaryWord[]>
+                (service.BeginMatchInDict, service.EndMatchInDict);
+
+            Func<string, IObservable<DictionaryWord[]>> matchInWordNetByPrefix = 
+                term => matchInDict("wn", term, "prefix");
+            
+            // The grand composition connecting the user input with the web service
+            var result = (from term in input
+                          select matchInWordNetByPrefix(term))
+                      .Switch();
+            
+            // Synchronize with the UI thread and populate the ListBox or signal an error
+            
+            using (result.ObserveOn(listBox).Subscribe(
+                words => { 
+                    listBox.Items.Clear();
+                    listBox.Items.AddRange((from word in words select word.Word).ToArray());
+                }, 
+                ex => { 
+                    MessageBox.Show("An error occurred: " + ex.Message, form.Text, 
+                        MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                })) 
+            { 
+                Application.Run(form);
+            }
         }
     }
 }
